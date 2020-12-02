a7ba3640597bdcc09517d2aa0c27c5004fd540e2
H._ <71264083+h-i-r@users.noreply.github.com>
2020-12-02 16:11:20 +0500

    Update costFunction.m

--- Week 3/Programming Assignment/ex2/costFunction.m
+++ Week 3/Programming Assignment/ex2/costFunction.m
@@ -24,7 +24,6 @@ h_theta = sigmoid(X*theta);
 J = (1 / m) * ((-y' * log(h_theta)) - (1 - y)' * log(1 - h_theta));
 
 grad = (1 / m) * (h_theta - y)' * X;
-# take ' transpose wherever we take summation
 
 % =============================================================
 
