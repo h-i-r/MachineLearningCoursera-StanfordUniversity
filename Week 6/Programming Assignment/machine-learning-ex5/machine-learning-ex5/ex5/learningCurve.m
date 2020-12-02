a07dd3cdcd06ea72c66108503cc721bed81075c1
H._ <71264083+h-i-r@users.noreply.github.com>
2020-12-02 16:33:22 +0500

    Update learningCurve.m

--- Week 6/Programming Assignment/machine-learning-ex5/machine-learning-ex5/ex5/learningCurve.m
+++ Week 6/Programming Assignment/machine-learning-ex5/machine-learning-ex5/ex5/learningCurve.m
@@ -57,11 +57,9 @@ for i = 1:m
 	Xtrain =  X(1:i, :);
 	Ytrain = y(1:i);
 	theta = trainLinearReg(Xtrain,Ytrain,lambda);
-	error_train(i) = linearRegCostFunction(Xtrain,Ytrain,theta,0); # lambda = 0 for no reg
-	error_val(i) = linearRegCostFunction(Xval,yval,theta,0);# lambda = 0 for no reg
+	error_train(i) = linearRegCostFunction(Xtrain,Ytrain,theta,0); 
+	error_val(i) = linearRegCostFunction(Xval,yval,theta,0);
 end
-
-% -------------------------------------------------------------
 
 % =========================================================================
 
